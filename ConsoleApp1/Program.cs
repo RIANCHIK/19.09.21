94fc9ad93d5cff14e83ea2d9d8c1570ee6702444
Gumbilier <kassano02@mail.ru>
2021-09-24 23:17:10 +0300

    version 0.1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а= ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите b= ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите c= ");
+            double c = double.Parse(Console.ReadLine());
+
+            double D = Math.Pow(b, 2) - 4 * (a * c);
+
+            double x1 = -b + Math.Sqrt(D) / 2 * a;
+            double x2 = -b - Math.Sqrt(D) / 2 * a;
+
+            if (D < 0)
+            {
+                Console.WriteLine("Нету корней");
+
+            }
+            else
+            {
+                if (D == 0)
+                {
+                    Console.WriteLine(x1);
+                }
+                else
+                {
+                    Console.WriteLine($"Корень первый будет равен {x1} | Корень второго равен {x2}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
